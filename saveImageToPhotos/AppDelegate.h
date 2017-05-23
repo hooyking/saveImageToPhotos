9771c70642ea39ba942a006e09d96d8ec16ae4bb
JingDong Xie <326871884@qq.com>
2017-05-23 17:57:40 +0800

    Initial Commit

---
+++ saveImageToPhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  saveImageToPhotos
+//
+//  Created by hooyking on 2017/5/23.
+//  Copyright © 2017年 hooyking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ saveImageToPhotos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  saveImageToPhotos
+//
+//  Created by hooyking on 2017/5/23.
+//  Copyright © 2017年 hooyking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
